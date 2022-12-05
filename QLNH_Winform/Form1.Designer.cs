6c8f445905ccb10bbf79629325780d5b076a9a8e
Andrew Nguyen <21520144@gm.uit.edu.vn>
2022-12-05 11:35:58 +0700

    Add project files.

---
+++ QLNH_Winform/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace QLNH_Winform
+{
+    partial class FormMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainMenu));
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.btnKhoHang = new System.Windows.Forms.Button();
+            this.btnNhanVien = new System.Windows.Forms.Button();
+            this.btnMonAn = new System.Windows.Forms.Button();
+            this.btnBanAn = new System.Windows.Forms.Button();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.pnLogo = new System.Windows.Forms.Panel();
+            this.pnTitleBar = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnDesktop = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnMenu.SuspendLayout();
+            this.pnLogo.SuspendLayout();
+            this.pnTitleBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.BackColor = System.Drawing.Color.White;
+            this.pnMenu.Controls.Add(this.btnKhoHang);
+            this.pnMenu.Controls.Add(this.btnNhanVien);
+            this.pnMenu.Controls.Add(this.btnMonAn);
+            this.pnMenu.Controls.Add(this.btnBanAn);
+            this.pnMenu.Controls.Add(this.btnThongKe);
+            this.pnMenu.Controls.Add(this.pnLogo);
+            this.pnMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(330, 695);
+            this.pnMenu.TabIndex = 0;
+            // 
+            // btnKhoHang
+            // 
+            this.btnKhoHang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnKhoHang.FlatAppearance.BorderSize = 0;
+            this.btnKhoHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKhoHang.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKhoHang.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnKhoHang.Image = ((System.Drawing.Image)(resources.GetObject("btnKhoHang.Image")));
+            this.btnKhoHang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnKhoHang.Location = new System.Drawing.Point(0, 504);
+            this.btnKhoHang.Name = "btnKhoHang";
+            this.btnKhoHang.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnKhoHang.Size = new System.Drawing.Size(330, 101);
+            this.btnKhoHang.TabIndex = 5;
+            this.btnKhoHang.Text = "  Kho hàng";
+            this.btnKhoHang.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnKhoHang.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnKhoHang.UseVisualStyleBackColor = true;
+            this.btnKhoHang.Click += new System.EventHandler(this.btnKhoHang_Click);
+            // 
+            // btnNhanVien
+            // 
+            this.btnNhanVien.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnNhanVien.FlatAppearance.BorderSize = 0;
+            this.btnNhanVien.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNhanVien.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNhanVien.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnNhanVien.Image = ((System.Drawing.Image)(resources.GetObject("btnNhanVien.Image")));
+            this.btnNhanVien.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNhanVien.Location = new System.Drawing.Point(0, 403);
+            this.btnNhanVien.Name = "btnNhanVien";
+            this.btnNhanVien.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnNhanVien.Size = new System.Drawing.Size(330, 101);
+            this.btnNhanVien.TabIndex = 4;
+            this.btnNhanVien.Text = " Nhân viên";
+            this.btnNhanVien.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNhanVien.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNhanVien.UseVisualStyleBackColor = true;
+            this.btnNhanVien.Click += new System.EventHandler(this.btnNhanVien_Click);
+            // 
+            // btnMonAn
+            // 
+            this.btnMonAn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMonAn.FlatAppearance.BorderSize = 0;
+            this.btnMonAn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMonAn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMonAn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnMonAn.Image = ((System.Drawing.Image)(resources.GetObject("btnMonAn.Image")));
+            this.btnMonAn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMonAn.Location = new System.Drawing.Point(0, 302);
+            this.btnMonAn.Name = "btnMonAn";
+            this.btnMonAn.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnMonAn.Size = new System.Drawing.Size(330, 101);
+            this.btnMonAn.TabIndex = 3;
+            this.btnMonAn.Text = "   Món ăn";
+            this.btnMonAn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMonAn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMonAn.UseVisualStyleBackColor = true;
+            this.btnMonAn.Click += new System.EventHandler(this.btnMonAn_Click);
+            // 
+            // btnBanAn
+            // 
+            this.btnBanAn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnBanAn.FlatAppearance.BorderSize = 0;
+            this.btnBanAn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBanAn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBanAn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnBanAn.Image = ((System.Drawing.Image)(resources.GetObject("btnBanAn.Image")));
+            this.btnBanAn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBanAn.Location = new System.Drawing.Point(0, 201);
+            this.btnBanAn.Name = "btnBanAn";
+            this.btnBanAn.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnBanAn.Size = new System.Drawing.Size(330, 101);
+            this.btnBanAn.TabIndex = 2;
+            this.btnBanAn.Text = "   Bàn ăn";
+            this.btnBanAn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBanAn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnBanAn.UseVisualStyleBackColor = true;
+            this.btnBanAn.Click += new System.EventHandler(this.btnBanAn_Click);
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnThongKe.FlatAppearance.BorderSize = 0;
+            this.btnThongKe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThongKe.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThongKe.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnThongKe.Image = ((System.Drawing.Image)(resources.GetObject("btnThongKe.Image")));
+            this.btnThongKe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThongKe.Location = new System.Drawing.Point(0, 100);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.btnThongKe.Size = new System.Drawing.Size(330, 101);
+            this.btnThongKe.TabIndex = 1;
+            this.btnThongKe.Text = " Thống kê";
+            this.btnThongKe.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThongKe.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+            // 
+            // pnLogo
+            // 
+            this.pnLogo.BackColor = System.Drawing.Color.White;
+            this.pnLogo.Controls.Add(this.label1);
+            this.pnLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnLogo.ForeColor = System.Drawing.Color.Blue;
+            this.pnLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnLogo.Name = "pnLogo";
+            this.pnLogo.Size = new System.Drawing.Size(330, 100);
+            this.pnLogo.TabIndex = 0;
+            // 
+            // pnTitleBar
+            // 
+            this.pnTitleBar.BackColor = System.Drawing.SystemColors.Control;
+            this.pnTitleBar.Controls.Add(this.lblTitle);
+            this.pnTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnTitleBar.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pnTitleBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.pnTitleBar.Location = new System.Drawing.Point(330, 0);
+            this.pnTitleBar.Name = "pnTitleBar";
+            this.pnTitleBar.Size = new System.Drawing.Size(867, 100);
+            this.pnTitleBar.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(369, 30);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(101, 38);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "HOME";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnDesktop
+            // 
+            this.pnDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnDesktop.Location = new System.Drawing.Point(330, 100);
+            this.pnDesktop.Name = "pnDesktop";
+            this.pnDesktop.Size = new System.Drawing.Size(867, 595);
+            this.pnDesktop.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 81);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "QLNH";
+            // 
+            // FormMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 695);
+            this.Controls.Add(this.pnDesktop);
+            this.Controls.Add(this.pnTitleBar);
+            this.Controls.Add(this.pnMenu);
+            this.Name = "FormMainMenu";
+            this.pnMenu.ResumeLayout(false);
+            this.pnLogo.ResumeLayout(false);
+            this.pnLogo.PerformLayout();
+            this.pnTitleBar.ResumeLayout(false);
+            this.pnTitleBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Button btnKhoHang;
+        private System.Windows.Forms.Button btnNhanVien;
+        private System.Windows.Forms.Button btnMonAn;
+        private System.Windows.Forms.Button btnBanAn;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.Panel pnLogo;
+        private System.Windows.Forms.Panel pnTitleBar;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel pnDesktop;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ QLNH_Winform/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLNH_Winform
+{
+    public partial class FormMainMenu : Form
+    {
+        //Fields
+        private Button currentButton;
+        private Form activeForm;
+
+        //Constructor
+        public FormMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        //private Color SelectThemeColor()
+        //{
+
+        //}
+        private void ActivateButton(object btnSender)
+        {
+            if(btnSender != null)
+            {
+                if(currentButton != (Button)btnSender)
+                {
+                    DeactivateButton();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = Color.FromArgb(96, 93, 236);
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Segoe UI", 13.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    pnTitleBar.BackColor = Color.FromArgb(128, 128, 255);
+                    pnTitleBar.ForeColor = Color.White;
+                    pnLogo.BackColor = ThemeColor.ChangeColorBrightness(Color.FromArgb(128, 128, 255), 0.7);
+
+                }    
+            }    
+        }
+
+        private void DeactivateButton()
+        {
+            foreach(Control previousBtn in pnMenu.Controls)
+            {
+                if(previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.White;
+                    previousBtn.ForeColor = Color.FromArgb(96, 93, 236);
+                    previousBtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }    
+                
+            }    
+        }
+
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if(activeForm != null)
+            {
+                activeForm.Close();
+            }
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.pnDesktop.Controls.Add(childForm);
+            this.pnDesktop.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            lblTitle.Text = childForm.Text;
+
+        }
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormThongKe(), sender);
+        }
+
+        private void btnBanAn_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormBanAn(), sender);
+        }
+
+        private void btnMonAn_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormMonAn(), sender);
+        }
+
+        private void btnNhanVien_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormNhanVien(), sender);
+        }
+
+        private void btnKhoHang_Click(object sender, EventArgs e)
+        {
+            OpenChildForm(new Forms.FormKhoHang(), sender);
+        }
+    }
+}

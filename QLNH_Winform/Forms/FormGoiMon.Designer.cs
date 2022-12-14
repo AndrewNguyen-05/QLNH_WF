871af41e69d5a5a83c24b8a050eda1fb6a26b53d
Phuoc Phan <21520405@ms.uit.edu.vn>
2022-12-14 13:26:12 +0700

    Fix form narrowing error + Remember ,Des using Dock property

--- QLNH_Winform/Forms/FormGoiMon.Designer.cs
+++ QLNH_Winform/Forms/FormGoiMon.Designer.cs
@@ -32,6 +32,9 @@
             this.guna2GradientPanel2 = new Guna.UI2.WinForms.Guna2GradientPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.guna2GradientPanel4 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            this.btnAddFood = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.cbFood = new System.Windows.Forms.ComboBox();
+            this.cbCategory = new System.Windows.Forms.ComboBox();
             this.lsvBill = new System.Windows.Forms.ListView();
             this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colCount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -41,32 +44,27 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.flpTable = new System.Windows.Forms.FlowLayoutPanel();
-            this.cbCategory = new System.Windows.Forms.ComboBox();
-            this.cbFood = new System.Windows.Forms.ComboBox();
-            this.btnAddFood = new System.Windows.Forms.Button();
             this.guna2GradientPanel1.SuspendLayout();
             this.guna2GradientPanel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // guna2GradientPanel1
             // 
-            this.guna2GradientPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.guna2GradientPanel1.Controls.Add(this.guna2GradientPanel2);
             this.guna2GradientPanel1.Controls.Add(this.label1);
+            this.guna2GradientPanel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.guna2GradientPanel1.Location = new System.Drawing.Point(0, 0);
             this.guna2GradientPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.guna2GradientPanel1.Name = "guna2GradientPanel1";
-            this.guna2GradientPanel1.Size = new System.Drawing.Size(1173, 82);
+            this.guna2GradientPanel1.Size = new System.Drawing.Size(1185, 60);
             this.guna2GradientPanel1.TabIndex = 0;
             // 
             // guna2GradientPanel2
             // 
-            this.guna2GradientPanel2.Location = new System.Drawing.Point(11, 51);
+            this.guna2GradientPanel2.Location = new System.Drawing.Point(12, 64);
             this.guna2GradientPanel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.guna2GradientPanel2.Name = "guna2GradientPanel2";
-            this.guna2GradientPanel2.Size = new System.Drawing.Size(671, 55);
+            this.guna2GradientPanel2.Size = new System.Drawing.Size(755, 69);
             this.guna2GradientPanel2.TabIndex = 2;
             // 
             // label1
@@ -74,17 +72,14 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Tomato;
-            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Location = new System.Drawing.Point(14, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(191, 23);
+            this.label1.Size = new System.Drawing.Size(225, 28);
             this.label1.TabIndex = 0;
             this.label1.Text = "RESTAURANT ORDERS";
             // 
             // guna2GradientPanel4
             // 
-            this.guna2GradientPanel4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.guna2GradientPanel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(229)))), ((int)(((byte)(229)))));
             this.guna2GradientPanel4.BorderRadius = 30;
             this.guna2GradientPanel4.BorderThickness = 5;
@@ -95,29 +90,73 @@
             this.guna2GradientPanel4.Controls.Add(this.guna2GradientButton1);
             this.guna2GradientPanel4.Controls.Add(this.label4);
             this.guna2GradientPanel4.Controls.Add(this.label3);
-            this.guna2GradientPanel4.Location = new System.Drawing.Point(768, 51);
+            this.guna2GradientPanel4.Dock = System.Windows.Forms.DockStyle.Right;
+            this.guna2GradientPanel4.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            this.guna2GradientPanel4.Location = new System.Drawing.Point(609, 60);
             this.guna2GradientPanel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.guna2GradientPanel4.Name = "guna2GradientPanel4";
-            this.guna2GradientPanel4.Size = new System.Drawing.Size(395, 551);
+            this.guna2GradientPanel4.Size = new System.Drawing.Size(576, 726);
             this.guna2GradientPanel4.TabIndex = 3;
+            // 
+            // btnAddFood
+            // 
+            this.btnAddFood.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAddFood.BorderColor = System.Drawing.Color.RosyBrown;
+            this.btnAddFood.BorderRadius = 6;
+            this.btnAddFood.BorderThickness = 2;
+            this.btnAddFood.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddFood.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddFood.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddFood.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddFood.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnAddFood.FillColor = System.Drawing.Color.Transparent;
+            this.btnAddFood.FillColor2 = System.Drawing.Color.Transparent;
+            this.btnAddFood.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddFood.ForeColor = System.Drawing.Color.Brown;
+            this.btnAddFood.Location = new System.Drawing.Point(362, 52);
+            this.btnAddFood.Name = "btnAddFood";
+            this.btnAddFood.Size = new System.Drawing.Size(202, 67);
+            this.btnAddFood.TabIndex = 8;
+            this.btnAddFood.Text = "Thêm món ăn";
+            // 
+            // cbFood
+            // 
+            this.cbFood.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbFood.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbFood.FormattingEnabled = true;
+            this.cbFood.Location = new System.Drawing.Point(19, 100);
+            this.cbFood.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbFood.Name = "cbFood";
+            this.cbFood.Size = new System.Drawing.Size(220, 40);
+            this.cbFood.TabIndex = 7;
+            // 
+            // cbCategory
+            // 
+            this.cbCategory.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbCategory.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCategory.FormattingEnabled = true;
+            this.cbCategory.Location = new System.Drawing.Point(19, 52);
+            this.cbCategory.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbCategory.Name = "cbCategory";
+            this.cbCategory.Size = new System.Drawing.Size(220, 40);
+            this.cbCategory.TabIndex = 6;
+            this.cbCategory.SelectedIndexChanged += new System.EventHandler(this.cbCategory_SelectedIndexChanged);
             // 
             // lsvBill
             // 
-            this.lsvBill.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lsvBill.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lsvBill.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.colName,
             this.colCount,
             this.colPrice,
             this.colTotal});
-            this.lsvBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsvBill.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lsvBill.GridLines = true;
             this.lsvBill.HideSelection = false;
-            this.lsvBill.Location = new System.Drawing.Point(3, 86);
+            this.lsvBill.Location = new System.Drawing.Point(19, 171);
             this.lsvBill.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.lsvBill.Name = "lsvBill";
-            this.lsvBill.Size = new System.Drawing.Size(390, 356);
+            this.lsvBill.Size = new System.Drawing.Size(545, 362);
             this.lsvBill.TabIndex = 0;
             this.lsvBill.UseCompatibleStateImageBehavior = false;
             this.lsvBill.View = System.Windows.Forms.View.Details;
@@ -144,7 +183,7 @@
             // 
             // guna2GradientButton1
             // 
-            this.guna2GradientButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2GradientButton1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.guna2GradientButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
             this.guna2GradientButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
             this.guna2GradientButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
@@ -154,88 +193,56 @@
             this.guna2GradientButton1.FillColor2 = System.Drawing.Color.Tomato;
             this.guna2GradientButton1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2GradientButton1.ForeColor = System.Drawing.Color.White;
-            this.guna2GradientButton1.Location = new System.Drawing.Point(85, 494);
+            this.guna2GradientButton1.Location = new System.Drawing.Point(19, 600);
             this.guna2GradientButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.guna2GradientButton1.Name = "guna2GradientButton1";
-            this.guna2GradientButton1.Size = new System.Drawing.Size(307, 55);
+            this.guna2GradientButton1.Size = new System.Drawing.Size(545, 60);
             this.guna2GradientButton1.TabIndex = 5;
             this.guna2GradientButton1.Text = "Thanh Toán";
             // 
             // label4
             // 
-            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(181, 425);
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(347, 549);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(211, 66);
+            this.label4.Size = new System.Drawing.Size(217, 48);
             this.label4.TabIndex = 4;
             this.label4.Text = "1,500,000 VND";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // label3
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(22, 425);
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 549);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(134, 66);
+            this.label3.Size = new System.Drawing.Size(171, 49);
             this.label3.TabIndex = 3;
             this.label3.Text = "Tổng Tiền :";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // flpTable
             // 
-            this.flpTable.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
             this.flpTable.AutoScroll = true;
             this.flpTable.BackColor = System.Drawing.Color.White;
-            this.flpTable.Location = new System.Drawing.Point(11, 51);
+            this.flpTable.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flpTable.Location = new System.Drawing.Point(0, 60);
             this.flpTable.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.flpTable.Name = "flpTable";
-            this.flpTable.Size = new System.Drawing.Size(752, 551);
+            this.flpTable.Size = new System.Drawing.Size(537, 726);
             this.flpTable.TabIndex = 1;
-            // 
-            // cbCategory
-            // 
-            this.cbCategory.FormattingEnabled = true;
-            this.cbCategory.Location = new System.Drawing.Point(28, 4);
-            this.cbCategory.Name = "cbCategory";
-            this.cbCategory.Size = new System.Drawing.Size(121, 24);
-            this.cbCategory.TabIndex = 6;
-            this.cbCategory.SelectedIndexChanged += new System.EventHandler(this.cbCategory_SelectedIndexChanged);
-            // 
-            // cbFood
-            // 
-            this.cbFood.FormattingEnabled = true;
-            this.cbFood.Location = new System.Drawing.Point(28, 46);
-            this.cbFood.Name = "cbFood";
-            this.cbFood.Size = new System.Drawing.Size(121, 24);
-            this.cbFood.TabIndex = 7;
-            // 
-            // btnAddFood
-            // 
-            this.btnAddFood.Location = new System.Drawing.Point(187, 7);
-            this.btnAddFood.Name = "btnAddFood";
-            this.btnAddFood.Size = new System.Drawing.Size(105, 63);
-            this.btnAddFood.TabIndex = 8;
-            this.btnAddFood.Text = "Thêm món";
-            this.btnAddFood.UseVisualStyleBackColor = true;
-            this.btnAddFood.Click += new System.EventHandler(this.btnAddFood_Click);
             // 
             // FormGoiMon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1173, 612);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1185, 786);
             this.Controls.Add(this.flpTable);
             this.Controls.Add(this.guna2GradientPanel4);
             this.Controls.Add(this.guna2GradientPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimizeBox = false;
             this.Name = "FormGoiMon";
             this.Text = "Đặt món ăn";
             this.guna2GradientPanel1.ResumeLayout(false);
@@ -261,6 +268,6 @@
         private System.Windows.Forms.FlowLayoutPanel flpTable;
         private System.Windows.Forms.ComboBox cbFood;
         private System.Windows.Forms.ComboBox cbCategory;
-        private System.Windows.Forms.Button btnAddFood;
+        private Guna.UI2.WinForms.Guna2GradientButton btnAddFood;
     }
 }

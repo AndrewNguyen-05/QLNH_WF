4355bea241bf92c4fd896ff2219a9c0cbc7743b1
Andrew Nguyen <21520144@gm.uit.edu.vn>
2022-12-28 13:20:03 +0700

    Update noti when delete food category

--- QLNH_Winform/Forms/FormDanhMuc.cs
+++ QLNH_Winform/Forms/FormDanhMuc.cs
@@ -44,6 +44,10 @@ namespace QLNH_Winform.Forms
 
         private void btnDelCategory_Click(object sender, EventArgs e)
         {
+            if (MessageBox.Show("Bạn có chắc chắn muốn xóa danh mục món?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.Cancel)
+            {
+                return;
+            }
             if (dtgvCategory.SelectedCells.Count <= 0) return;
             foreach (DataGridViewCell dgvc in dtgvCategory.SelectedCells)
             {
